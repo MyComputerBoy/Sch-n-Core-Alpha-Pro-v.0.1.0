0d62b5238051c8cb1a4044580361cc84cd836adc
Kim Chemnitz <hatelag1@gmail.com>
2023-10-11 19:03:49 +0200

    Initial Implementation
    Far from done though, but a huge step forward

---
+++ BaseCPUInfo.py
@@ -0,0 +1,12 @@
+"""BaseCPUInfo.py -> Basic information about the project
+Main user variables:
+
+base_folder -> Root folder in which the project is stored in
+programs_folder -> Relative folder in which the source code is stored in
+executable_files_folder -> Relative folder in which the executables are stored in
+"""
+
+bit_width = 32																			#The width of the word
+base_folder = "C:/Users/Kim Chemnitz/Documents/GitHub/Sch-n-Core-Alpha-Pro-v.0.1.0/"	#The lowest commom folder
+programs_folder = "Documents/"															#Which folder where non compiled programs is to be found
+executable_files_folder = "Programs/"													#The folder where compiled programs is to be found 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Schön Core Alpha Pro v.0.1.0
  Schön Core Alpha Pro v.0.1.0 Python Edition
+ 
+ My 32 bit single core CPU
