d3ee0a333422427dd4220a54530c1df431bd525c
Kim Chemnitz <hatelag1@gmail.com>
2025-12-11 11:40:17 +0100

    Made Symbols class more mature, still needs predefining everything we already know

--- Asmblr.py
+++ Asmblr.py
@@ -12,30 +12,70 @@ class Symbols():
 
 	def __init__(self: Self) -> None:
 		#Base container for all symbols, static and dynamic
-		self.SymbolGroups: dict[str, list[str]] = {}
+		#So, SymbolsGroups is a dict containing every category, which is also a dict
+		#And the categories has lists of all the elements that you wanna use
+		self.SymbolGroups: dict[str, dict[str, list[str]]] = {}
 
-		self.CreateSymbolsGroup("FunctionsSymbols")
-		self.CreateSymbolsGroup("ComparisonSymbols")
-		self.CreateSymbolsGroup("ArithmeticLogicSymbols")
+		# DidCreatePredefinedGroups: bool = self.CreatePredefinedGroups()
+		# if not DidCreatePredefinedGroups:
+		# 	raise Exception("Could not create predefined symbols.")
+
+	def __setitem__(self: Self, GroupName: str, CategoryName: str, Value: str) -> None:
+		self.SymbolGroups[GroupName][CategoryName].append(Value)
 	
-	def __setitem__(self: Self, Key: str, Value: str) -> None:
-		self.SymbolGroups[Key].append(Value)
+	def __getitem__(self: Self, GroupName: str, CategoryName: str, Index: int) -> str:
+		return self.SymbolGroups[GroupName][CategoryName][Index]
 	
-	def __getitem__(self: Self, Key: str, Index: int) -> str:
-		return self.SymbolGroups[Key][Index]
-	
+	def CreatePredefinedGroups(self: Self, GroupName: str, CategoryNames: list[str], PopulationList: list[list[str]]) -> bool:
+		#Creating the group
+		DidCreateGroupCategory: bool = self.CreateSymbolsGroup(GroupName)
+		if not DidCreateGroupCategory:
+			raise Exception("Could not create $s group." % (GroupName))
+		
+		#Creating the categories
+		for Category in CategoryNames:
+			DidCreateGroupCategory: bool = self.CreateSymbolGroupsCategory(GroupName, Category)
+			if not DidCreateGroupCategory:
+				raise Exception("Could not create %s[%s] category." % (GroupName, Category))
+		
+		#Populating the categories in the group
+		PopulationIndex: int = 0
+		for PopulationListElement in PopulationList:
+			DidPopulateCategory: bool = self.PopulateWholeCategoryFromList(GroupName, CategoryNames[PopulationIndex], PopulationListElement)
+			if not DidPopulateCategory:
+				raise Exception("Could not populate %s[$s] category." % (GroupName, CategoryNames[PopulationIndex]))
+			PopulationIndex += 1
+
+		return True
+
+	def PopulateWholeCategoryFromList(self: Self, GroupName: str, CategoryName: str, PopulatedCategory: list[str]) -> bool:
+		try:
+			self.SymbolGroups[GroupName][CategoryName] = PopulatedCategory
+
+			return True
+		except Exception:
+			return False
+
 	def CreateSymbolsGroup(self: Self, GroupName: str) -> bool:
 		try:
-			self.SymbolGroups[GroupName] = []
+			self.SymbolGroups[GroupName] = {}
 			return True
 		except Exception:
 			return False
 	
-	def DeleteSymbolsGroup(self: Self, Key: str) -> bool:
+	def DeleteSymbolsGroup(self: Self, GroupName: str) -> bool:
 		try:
-			del self.SymbolGroups[Key]
+			del self.SymbolGroups[GroupName]
 			return True
 		except KeyError:
+			return False
+	
+	def CreateSymbolGroupsCategory(self: Self, GroupName: str, CategoryName: str) -> bool:
+		try:
+			self.SymbolGroups[GroupName][CategoryName] = []
+
+			return True
+		except Exception:
 			return False
 
 class Assembler: #Main assembler class to assemble
@@ -62,3 +102,24 @@ class Assembler: #Main assembler class to assemble
 		self.RawBinaryOutputFile: list[str] = []	#Raw output file in raw binary
 
 		self.SymbolsHandler: "Symbols" = Symbols()
+	
+	def LoadAssemblyFileToClass(self: Self, PathToAssemblyFile: str) -> bool:
+		try:
+			FileHandler = open(PathToAssemblyFile, "r")
+			self.AssemblyFileRead = FileHandler.readlines()
+
+			return True
+		except FileNotFoundError:
+			return False
+	
+	# def ParseSymbols(self: Self) -> bool:
+	# 	WorkingLineIndex: int = 0
+
+	# 	#Go through every line to parse
+	# 	for TemporaryWorkingLine in self.AssemblyFileRead:
+	# 		SpaceSeperatedWorkingLine: list[str] = TemporaryWorkingLine.split(" ")
+
+	# 		#Make sure the WorkingLineIndex reflects which line is worked on
+	# 		WorkingLineIndex += 1
+
+	# 	return True
